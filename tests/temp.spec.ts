b82596a1bbfe05e5b097518e6edccba55a72a086
Saurabh Pandey <saurabhpnd36@gmail.com>
2026-08-06 02:07:26 +0530

    Child Window Handling| JS POP Ups

--- tests/temp.spec.ts
+++ tests/temp.spec.ts
@@ -1,11 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { chromium, test, expect } from '@playwright/test';
+//Below test setup browser , then its context and then pages
+test.skip('Browser envoke - Google', async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const pages = await context.newPage();
+  const pages1 = await context.newPage();
+  await pages.goto('https://www.google.com');
+  await expect(pages).toHaveTitle('Google');
+  await pages1.goto('https://www.facebook.com');
+  await expect(pages1).toHaveTitle('Facebook')
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.amazon.com/');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('laptop');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
-  await page.getByRole('link', { name: 'Samsung 14" Galaxy Chromebook' }).click();
-  await page.getByRole('button', { name: 'Add to cart', exact: true }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-});
+})
+test.skip('Child Window', async({page})=>{
+  const parent = page;
+  await page.goto('https://testautomationpractice.blogspot.com/')
+  const [child] = await Promise.all([page.waitForEvent('popup'), page.getByRole('button', {name: 'New Tab'}).click()])
+  await child.waitForLoadState();
+  const total_page = page.context().pages();
+  await console.log(total_page.length);
+  await expect(child).toHaveURL('https://www.pavantestingtools.com/');
+  await parent.bringToFront();
+  await child.close();
+})
+test.skip('Entering details in JS POP UP', async()=>{
+  const browser = await chromium.launch();
+  const context = await browser.newContext({httpCredentials: {username: 'admin', password: 'admin'}});
+  const page = await context.newPage();
+  await page.goto('https://the-internet.herokuapp.com/basic_auth');
+  await page.waitForLoadState();
+  await expect(page.locator('text= Basic Auth')).toBeVisible();
+})
